e4dd4affbc7a6bb1cb04c8394e7d23474c615814
cordyho <terminator77587@gmail.com>
2019-08-19 16:03:29 +0800

    2019 8 19

--- app/src/main/java/com/cordy/secdra/module/main/adapter/PictureRvAdapter.kt
+++ app/src/main/java/com/cordy/secdra/module/main/adapter/PictureRvAdapter.kt
@@ -1,7 +1,6 @@
 package com.cordy.secdra.module.main.adapter
 
 import android.annotation.SuppressLint
-import android.util.SparseArray
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.cordy.secdra.R
@@ -12,11 +11,9 @@ import com.cordy.secdra.widget.ScaleImageView
 @SuppressLint("UseSparseArrays")
 class PictureRvAdapter : BaseQuickAdapter<JsonBeanPicture.DataBean.ContentBean, BaseViewHolder>(R.layout.item_rv_picture) {
 
-    private val heightMap = SparseArray<Int?>()
-
     override fun convert(helper: BaseViewHolder, item: JsonBeanPicture.DataBean.ContentBean) {
         val ivPicture = helper.getView<ScaleImageView>(R.id.iv_picture)
-        ivPicture.setInitSize(item.width, item.height)
+        ivPicture.setInitSize(item.width, item.height)  //重写IV的测量方法，设置图片宽高缩放到屏幕实际的宽高
         ImageLoader.setBaseImageFromUrl(item.url, ivPicture)
     }
 }
