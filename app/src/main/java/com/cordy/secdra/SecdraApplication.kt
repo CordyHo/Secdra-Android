c8669e38497564165c9d74eb64e5faf1955b0374
cordyho <terminator77587@gmail.com>
2019-09-03 10:43:43 +0800

    2019 9 2

--- app/src/main/java/com/cordy/secdra/SecdraApplication.kt
+++ app/src/main/java/com/cordy/secdra/SecdraApplication.kt
@@ -29,7 +29,7 @@ class SecdraApplication : Application() {
     }
 
     private fun initCrashReport() {
-        val appID = "e9ee7a2284"
+        val appID = "c0eef43ba8"
         val packageName = application?.packageName
         val processName = getProcessName(android.os.Process.myPid())
         val strategy = CrashReport.UserStrategy(application)
