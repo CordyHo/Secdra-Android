9a400737d4ed323189cf3010ee1257df02081364
cordyho <terminator77587@gmail.com>
2019-09-16 16:35:57 +0800

    2019 9 16

--- app/src/main/java/com/cordy/secdra/utils/ToastUtil.kt
+++ app/src/main/java/com/cordy/secdra/utils/ToastUtil.kt
@@ -4,29 +4,30 @@ import android.widget.Toast
 import com.cordy.secdra.SecdraApplication
 
 object ToastUtil {
-    private var oldMsg: String? = null
+
+    private var oldMsg: Any? = null
     private var time: Long = 0
 
-    fun showToastShort(msg: String?) {
+    fun showToastShort(msg: Any?) {
         if (msg != oldMsg) {
-            Toast.makeText(SecdraApplication.application, msg, Toast.LENGTH_SHORT).show()
+            Toast.makeText(SecdraApplication.application, msg.toString(), Toast.LENGTH_SHORT).show()
             time = System.currentTimeMillis()
         } else {
             if (System.currentTimeMillis() - time > 1500) {
-                Toast.makeText(SecdraApplication.application, msg, Toast.LENGTH_SHORT).show()
+                Toast.makeText(SecdraApplication.application, msg.toString(), Toast.LENGTH_SHORT).show()
                 time = System.currentTimeMillis()
             }
         }
         oldMsg = msg
     }
 
-    fun showToastLong(msg: String?) {
+    fun showToastLong(msg: Any?) {
         if (msg != oldMsg) {
-            Toast.makeText(SecdraApplication.application, msg, Toast.LENGTH_LONG).show()
+            Toast.makeText(SecdraApplication.application, msg.toString(), Toast.LENGTH_LONG).show()
             time = System.currentTimeMillis()
         } else {
             if (System.currentTimeMillis() - time > 1500) {
-                Toast.makeText(SecdraApplication.application, msg, Toast.LENGTH_LONG).show()
+                Toast.makeText(SecdraApplication.application, msg.toString(), Toast.LENGTH_LONG).show()
                 time = System.currentTimeMillis()
             }
         }
