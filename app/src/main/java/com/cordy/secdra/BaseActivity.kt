4efeb9aae4bb3f4ce694b837b0a75d7805ac6e38
cordyho <terminator77587@gmail.com>
2019-09-24 17:05:27 +0800

    2019 9 24

--- app/src/main/java/com/cordy/secdra/BaseActivity.kt
+++ app/src/main/java/com/cordy/secdra/BaseActivity.kt
@@ -1,15 +1,21 @@
 package com.cordy.secdra
 
+import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.cordy.secdra.utils.ActivityStackManager
 
 abstract class BaseActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityStackManager.addToStack(this)
+    }
+
     open fun initView() {
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        toolbar?.run {
-            setSupportActionBar(toolbar)
+        findViewById<Toolbar>(R.id.toolbar)?.let {
+            setSupportActionBar(it)
             supportActionBar?.setDisplayShowTitleEnabled(false)
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
@@ -20,5 +26,10 @@ abstract class BaseActivity : AppCompatActivity() {
             android.R.id.home -> onBackPressed()
         }
         return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ActivityStackManager.onDestroyRemove(this)
     }
 }
---
+++ app/src/main/java/com/cordy/secdra/utils/ActivityStackManager.kt
@@ -0,0 +1,40 @@
+package com.cordy.secdra.utils
+
+import android.app.Activity
+
+object ActivityStackManager {
+
+    private val stackList = ArrayList<Activity>()
+
+    fun addToStack(activity: Activity) {
+        stackList.add(activity)
+    }
+
+    fun onDestroyRemove(activity: Activity?) {
+        activity?.let {
+            if (!it.isFinishing)
+                it.finish()
+        }
+        stackList.removeAt(stackList.indexOf(activity))
+    }
+
+    fun removeByName(className: Class<*>) {  //这里不需要remove List，因为activity finish的时候会执行在onDestroy()里执行 onDestroyRemove() 方法，防止数组越界
+        for (activity in stackList)
+            if (activity.javaClass == className) {
+                activity.finish()
+                break
+            }
+    }
+
+    fun getCurrentTopActivity(): Activity? {
+        return stackList.lastOrNull()
+    }
+
+    fun getBottomActivity(): Activity? {
+        return stackList.firstOrNull()
+    }
+
+    fun getActivityCount(): Int {
+        return stackList.size
+    }
+}
