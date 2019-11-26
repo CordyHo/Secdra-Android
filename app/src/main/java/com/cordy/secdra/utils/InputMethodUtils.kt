cfa8f3dc73df57cd0e5b1ebfe38ac3de878cc68c
cordyho <terminator77587@gmail.com>
2019-11-26 15:41:54 +0800

    2019 11 19

--- app/src/main/java/com/cordy/secdra/utils/InputMethodUtils.kt
+++ app/src/main/java/com/cordy/secdra/utils/InputMethodUtils.kt
@@ -7,13 +7,13 @@ import com.cordy.secdra.SecdraApplication
 
 object InputMethodUtils {
     fun showInputMethod(editText: EditText) {
-        val inputManager = SecdraApplication.application?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputManager.showSoftInput(editText, InputMethodManager.SHOW_FORCED)
+        val inputManager = SecdraApplication.application?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        inputManager?.showSoftInput(editText, InputMethodManager.SHOW_FORCED)
     }
 
     fun closeInputMethod(editText: EditText) {
-        val imm = SecdraApplication.application?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(editText.windowToken, 0)
+        val imm = SecdraApplication.application?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(editText.windowToken, 0)
         editText.clearFocus()
     }
 }
