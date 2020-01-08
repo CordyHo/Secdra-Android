053619dbbcc5519a8881068f2be99e54e9db2d89
cordyho <terminator77587@gmail.com>
2020-01-08 10:50:52 +0800

    2020 1 7

--- app/src/main/java/com/cordy/secdra/utils/SavePictureUtils.kt
+++ app/src/main/java/com/cordy/secdra/utils/SavePictureUtils.kt
@@ -1,16 +1,9 @@
-@file:Suppress("DEPRECATION")
-
 package com.cordy.secdra.utils
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues
-import android.media.MediaScannerConnection
 import android.net.Uri
-import android.os.Build
-import android.os.Environment
 import android.provider.MediaStore
-import android.webkit.MimeTypeMap
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -22,64 +15,38 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.OutputStream
 
-
-@SuppressLint("InlinedApi")
 object SavePictureUtils {
-
-    private var PICTURE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).path + "/"
 
     fun savePicture(context: Activity?, url: String?, headOrBgUrl: String = "") {   // headOrBgUrl 保存的是头像或者背景图时传入
 
         object : Thread() {
             override fun run() {
-                if (Build.VERSION.SDK_INT >= 29)
-                    savePictureFor10Up(context, url, headOrBgUrl)
-                else
-                    try {
-                        savePictureFor10Down(context, url, headOrBgUrl)
-                        context?.runOnUiThread { ToastUtil.showToastLong(context.getString(R.string.saveSuccess)) }
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        context?.runOnUiThread { ToastUtil.showToastLong(context.getString(R.string.saveFailure)) }
-                    }
+                // Android 10之后不允许使用File类来操作公有文件目录，建议用ContentResolver和MediaStore，大概可以兼容全版本
+                val resolver = context?.contentResolver
+                val values = ContentValues()  //配置需要保存图片的相关属性
+                values.put(MediaStore.Images.Media.DISPLAY_NAME, "$url.jpg")  //设置文件名字，貌似只有Android 10 才能正常显示文件名
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")  //设置文件类型为image，貌似默认保存到Pictures这个文件夹里
+                val insertUri: Uri? = resolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)  //生成要保存的文件的uri，即路径
+                var inStream: FileInputStream? = null
+                var outStream: OutputStream? = null
+                try {
+                    var byteRead: Int?
+                    val cacheFileUri = getGlidePictureFromCache(url, headOrBgUrl)?.path  //得到Glide缓存图片，要在后台线程执行
+                    insertUri?.let { outStream = resolver.openOutputStream(insertUri) }  //要保存的图片生成一个输出流，即一个空的文件
+                    cacheFileUri?.let { inStream = FileInputStream(cacheFileUri) }   //原图片生成输入流
+                    val buffer = ByteArray(1024)
+                    while (inStream?.read(buffer).also { byteRead = it } != -1)
+                        byteRead?.let { outStream?.write(buffer, 0, it) }   //写入要保存的图片
+                    context?.runOnUiThread { ToastUtil.showToastLong(context.getString(R.string.saveSuccess)) }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    context?.runOnUiThread { ToastUtil.showToastLong(context.getString(R.string.saveFailure)) }
+                } finally {
+                    inStream?.close()
+                    outStream?.close()
+                }
             }
         }.start()
-    }
-
-    private fun savePictureFor10Down(context: Activity?, url: String?, headOrBgUrl: String = "") {  //Android 10以下
-        createPictureFolder()
-        val newFile = File("$PICTURE_PATH$url.jpg")  // 新建一个复制缓存图片的文件
-        val cacheFile = getGlidePictureFromCache(url, headOrBgUrl)  //得到Glide缓存图片，要在后台线程执行
-        cacheFile?.copyTo(newFile, true)   //把缓存图片复制到新文件
-        //更新相册
-        MediaScannerConnection.scanFile(context, arrayOf(newFile.absolutePath), arrayOf(MimeTypeMap.getSingleton().getMimeTypeFromExtension("jpg")), null)
-    }
-
-    private fun savePictureFor10Up(context: Activity?, url: String?, headOrBgUrl: String = "") {   // Android 10以上
-        // Android 10之后不允许使用File类来操作公有文件目录，要用ContentResolver和MediaStore
-        val resolver = context?.contentResolver
-        val values = ContentValues()  //配置需要保存图片的相关属性
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, "$url.jpg")  //设置文件名字
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/$url.jpg")  //设置文件类型为image，貌似默认保存到Pictures这个文件夹里
-        val insertUri: Uri? = resolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)  //生成要保存的文件的uri，即路径
-        var inStream: FileInputStream? = null
-        var outStream: OutputStream? = null
-        try {
-            var byteRead: Int?
-            val cacheFileUri = getGlidePictureFromCache(url, headOrBgUrl)?.path  //得到Glide缓存图片，要在后台线程执行
-            insertUri?.let { outStream = resolver.openOutputStream(insertUri) }  //要保存的图片生成一个输出流，即一个空的文件
-            cacheFileUri?.let { inStream = FileInputStream(cacheFileUri) }   //原图片生成输入流
-            val buffer = ByteArray(1024)
-            while (inStream?.read(buffer).also { byteRead = it } != -1)
-                byteRead?.let { outStream?.write(buffer, 0, it) }   //写入要保存的图片
-            context?.runOnUiThread { ToastUtil.showToastLong(context.getString(R.string.saveSuccess)) }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            context?.runOnUiThread { ToastUtil.showToastLong(context.getString(R.string.saveFailure)) }
-        } finally {
-            inStream?.close()
-            outStream?.close()
-        }
     }
 
     private fun getGlidePictureFromCache(url: Any?, headOrBgUrl: String? = ""): File? {
@@ -106,10 +73,5 @@ object SavePictureUtils {
                 null
             }
         } ?: run { return null }
-    }
-
-    private fun createPictureFolder(): Boolean {
-        val file = File(PICTURE_PATH)
-        return file.exists() || file.mkdirs()
     }
 }
